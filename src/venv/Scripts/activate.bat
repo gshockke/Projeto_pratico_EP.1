64c9b60b401cbf32888389df328f1beb7b3b3ee7
Paulista <41450122+gshockke@users.noreply.github.com>
2022-11-21 13:07:47 -0300

    troca arquivo sstyle para style, href do base html

--- src/venv/Scripts/activate.bat
+++ src/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Documentos\A FATEC\DSM\Design Digital\Git exercicios\Projeto Prático EP.1\src\venv
+set VIRTUAL_ENV=D:\Documentos\A FATEC\DSM\Design Digital\Git exercicios\Projeto Prático EP.1\Projeto_pratico_EP.1\src\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
