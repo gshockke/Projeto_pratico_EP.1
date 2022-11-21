a58476c940af5ff8903c829c20d32a652a4d31e5
Paulista <41450122+gshockke@users.noreply.github.com>
2022-11-21 00:11:22 -0300

    Acertando sstyle.css para as outras paginas

--- src/app.py
+++ src/app.py
@@ -3,23 +3,23 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
+@app.route("/base")
+def base():
     return render_template("base.html")
 
-# @app.route("/home")
-# def home():
-#     return render_template("home.html")
+@app.route('/home')
+def home():
+    return render_template("home.html")
 
 @app.route('/resumo')
 def resumo():
     return render_template("resumo.html")
 
-@app.route("/formacao")
+@app.route('/formacao')
 def formacao():
     return render_template("formacao.html")
 
-@app.route("/historico")
+@app.route('/historico')
 def historico():
     return render_template("historico.html")
 
